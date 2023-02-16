98d17d535834192596c849f34858ba3365a6f184
Kibugi1 <njugunabrian843@gmail.com>
2023-02-16 09:01:00 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main- printing using printf
+ * Description: This is taask four
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
