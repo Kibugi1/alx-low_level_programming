081e1cb8963ee05688b937beed9e2981bf179072
Kibugi1 <njugunabrian843@gmail.com>
2023-04-03 13:06:30 -0700

    This is Task 0; Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * print_listint - prints the elements of a list
+ * @h: the linked list of type to print
+ * Return: The number of nodes in list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodenum = 0;
+
+	while (h)
+	{
+	printf("%d\n", h->n);
+	nodenum++;
+	h = h->next;
+	}
+
+	return (nodenum);
+
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,7 @@
+These are ALX tasks on more singly linked lists
+How to use linked lists
+Start to look for the right source of information without too much help
+ prints all the elements of a given  list
+Write a function that returns the number of elements in a list
+And more functions related to linked lists.
+
