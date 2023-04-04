9863b518773ae717243a75dd2a106f0208f51bde
Kibugi1 <njugunabrian843@gmail.com>
2023-04-04 09:00:24 -0700

    This is a correction commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,15 +10,15 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int num;
+	int n;
 
 	if (!head || *head)
 	return (0);
 
-	num = (*head)->n;
+	n = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
 	*head = temp;
 
-	return (num);
+	return (n);
 }
