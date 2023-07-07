a517a64bbe9ae46335daefe70d8d59bd0c4494db
Kibugi1 <njugunabrian843@gmail.com>
2023-07-07 04:55:54 -0700

    Task 2 : Solved betty error

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,7 +3,7 @@
 /**
  * key_index - A function that gives you the index of a key using the
  * hash_djb2 function
-
+ *
  * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
  *
