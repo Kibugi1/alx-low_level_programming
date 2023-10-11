f4b3997c418bb919544fa74adccc2079880c3f98
Kibugi1 <njugunabrian843@gmail.com>
2023-10-11 09:48:56 -0700

    Task 1 and 2: linear and binary search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,18 @@
+#ifndef __SEARCH_ALGOS__
+#define __SEARCH_ALGOS__
+
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int _binary_searching(int *array, size_t start, size_t end, int value);
+int advanced_binary(int *array, size_t size, int value);
+int recursive_binary_search(int *array, size_t start, size_t end, int value);
+
+#endif /* __SEARCH_ALGOS__ */
